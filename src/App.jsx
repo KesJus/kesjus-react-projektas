41cf499c5ed39f5bae60044233f9852ac769d122
KestasJ <kesjus@gmail.com>
2023-04-20 11:33:10 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './styles/reset.css';
 import './styles/App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+// import Header from './components/layout/Header';
+// import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className='container'>
-      <h1>My Shop</h1>
+    <div className="">
+      {/* <Header /> */}
+      {/* <Toaster /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/add" element={<AddShopPage />} />
+        <Route path="/shop" element={<ShopPage />} /> */}
+      </Routes>
     </div>
   );
 }
